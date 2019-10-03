b739ebe546cbbebdb8d1720738fff02632304471
AlekseyMikheev <dagas_88@mail.ru>
2019-10-03 18:50:16 +0300

    Revert "Add draggable"
    This reverts commit 3c506bc78c73dba74f905e6d5ed2250f8e65f157.

--- js/map.js
+++ js/map.js
@@ -5,9 +5,6 @@
   var ITEMS_COUNT = 8;
   var MAP_PIN_MAIN_AFTER_HEIGHT = 22;
 
-  var MIN_MAP_Y = 130;
-  var MAX_MAP_Y = 630;
-
   var dataModule = window.data;
   var pinModule = window.pin;
   var cardModule = window.card;
@@ -20,16 +17,9 @@
   var adForm = document.querySelector('.ad-form');
   var mapFiltersForm = document.querySelector('.map__filters');
   var cardTemplate = document.querySelector('#card');
-
   var mapPins = document.querySelector('.map__pins');
-  var minMapX = mapPins.offsetLeft;
-  var maxMapX = mapPins.offsetLeft + mapPins.offsetWidth - mapPinMain.offsetWidth;
-
   var mapFiltersContainer = document.querySelector('.map__filters-container');
   var pinTemplate = document.querySelector('#pin');
-
-  var isMapEnabled = false;
-  var draggedMapPinButton = null;
 
   var onOfferCardClose = function () {
     hideOfferCard();
@@ -49,27 +39,19 @@
     isOfferCardOpened = true;
   };
 
-
   var addPinClickEvent = function (pins) {
     mapPins.addEventListener('click', function (evt) {
-      if (draggedMapPinButton == null) {
-        console.log('click')
-        var mapPinButton = null;
+      var mapPinButton = null;
 
-        if (evt.target.classList.contains('map__pin') && !evt.target.classList.contains('map__pin--main')) {
-          mapPinButton = evt.target;
-        } else if (evt.target.parentElement.classList.contains('map__pin') && !evt.target.parentElement.classList.contains('map__pin--main')) {
-          mapPinButton = evt.target.parentElement;
-        }
-
-        if (mapPinButton !== null && mapPinButton.dataset !== null) {
-          var index = mapPinButton.dataset.index;
-          showOfferCard(pins[index]);
-        }
+      if (evt.target.classList.contains('map__pin') && !evt.target.classList.contains('map__pin--main')) {
+        mapPinButton = evt.target;
+      } else if (evt.target.parentElement.classList.contains('map__pin') && !evt.target.parentElement.classList.contains('map__pin--main')) {
+        mapPinButton = evt.target.parentElement;
       }
-      else {
-        evt.preventDefault();
-        draggedMapPinButton = null;
+
+      if (mapPinButton !== null && mapPinButton.dataset !== null) {
+        var index = mapPinButton.dataset.index;
+        showOfferCard(pins[index]);
       }
     });
   };
@@ -115,7 +97,6 @@
   };
 
   var enableMap = function () {
-    isMapEnabled = true;
     map.classList.remove('map--faded');
 
     adForm.classList.remove('ad-form--disabled');
@@ -127,7 +108,6 @@
   };
 
   var disableMap = function () {
-    isMapEnabled = false;
     map.classList.add('map--faded');
 
     adForm.classList.add('ad-form--disabled');
@@ -138,80 +118,12 @@
     addElementsAttribute(mapFiltersForm, 'fieldset', 'disabled', true);
   };
 
-  // var updateAddress = function () {
-  //   var address = getAddress(mapPinMain.offsetLeft, mapPinMain.offsetTop, mapPinMain.offsetWidth, mapPinMain.offsetHeight);
-  //   if (isAddressValid(address)) {
-  //     formModule.setAddress(address.x + ' ' + address.y);
-  //   }
-  // }
-
-  var getAddress = function (left, top, width, height) {
-    var addressX = Math.round(left + (width / 2));
-    var addressY = Math.round(top + (height + (MAP_PIN_MAIN_AFTER_HEIGHT / 2)));
-    return { x: addressX, y: addressY };
-  };
-
-  var isAddressValid = function (address) {
-    return address.x > minMapX && address.x < maxMapX && address.y > MIN_MAP_Y && address.y < MAX_MAP_Y;
-  };
-
   var initMapPinMainEvents = function () {
     mapPinMain.addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
-
-      if (!isMapEnabled) {
-        enableMap();
-      }
-
-      var dragged = false;
-
-      var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-      };
-
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-
-        dragged = true;
-        draggedMapPinButton = mapPinMain;
-
-        var address = getAddress(mapPinMain.offsetLeft, mapPinMain.offsetTop, mapPinMain.offsetWidth, mapPinMain.offsetHeight);
-        if (isAddressValid(address)) {
-          formModule.setAddress(address.x + ' ' + address.y);
-
-          var shift = {
-            x: startCoords.x - moveEvt.clientX,
-            y: startCoords.y - moveEvt.clientY
-          };
-
-          startCoords = {
-            x: moveEvt.clientX,
-            y: moveEvt.clientY
-          };
-
-          var top = mapPinMain.offsetTop - shift.y;
-          var left = mapPinMain.offsetLeft - shift.x;
-
-          mapPinMain.style.top = top + 'px';
-          mapPinMain.style.left = left + 'px';
-        }
-
-        var onMouseUp = function (upEvt) {
-          upEvt.preventDefault();
-
-          var address = getAddress(mapPinMain.offsetLeft, mapPinMain.offsetTop, mapPinMain.offsetWidth, mapPinMain.offsetHeight);
-          if (isAddressValid(address)) {
-            formModule.setAddress(address.x + ' ' + address.y);
-          }
-
-          document.removeEventListener('mousemove', onMouseMove);
-          document.removeEventListener('mouseup', onMouseUp);
-
-        };
-        document.addEventListener('mouseup', onMouseUp);
-      }
-      document.addEventListener('mousemove', onMouseMove);
+      var addressX = Math.round(evt.currentTarget.offsetLeft + (evt.currentTarget.offsetWidth / 2));
+      var addressY = Math.round(evt.currentTarget.offsetTop + (evt.currentTarget.offsetHeight + (MAP_PIN_MAIN_AFTER_HEIGHT / 2)));
+      formModule.setAddress(addressX + ' ' + addressY);
+      enableMap();
     });
   };
 
